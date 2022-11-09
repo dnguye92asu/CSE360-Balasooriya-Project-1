cc4526ba1f381d39cf8531e59a11d404501f8989
dnguye92asu <dnguye92@asu.edu>
2022-11-09 00:05:17 -0700

    Innitial login and order UI

---
+++ Phase III/src/Login.java
@@ -0,0 +1,61 @@
+import java.util.Objects;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+    private StackPane mainPane = new StackPane();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+
+        Label role = new Label("What's your role at ASU's Pizza"); // ask to display appropriate UI
+        Button studentButton = new Button("Student");
+        Button staffButton = new Button("Staff");
+
+        StackPane.setAlignment(role, Pos.CENTER);
+        StackPane.setMargin(studentButton, new Insets(50, 60, 0, 0));
+        StackPane.setMargin(staffButton, new Insets(50, -60, 0, 0));
+
+        mainPane.getChildren().addAll(role, studentButton, staffButton);
+        primaryStage.setTitle("ASU Pizza");
+        primaryStage.setScene(new Scene(mainPane, 200, 200));
+        primaryStage.show();
+
+        studentButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // display pizza order UI as a new Window
+                // uses fxml file via SceneBuilder
+
+                try {
+                    Stage orderUI = new Stage();
+                    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("student_order.fxml")));
+                    orderUI.setTitle("Welcome to ASU's Pizza");
+                    orderUI.setScene(new Scene(root, 600, 400));
+                    orderUI.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                }
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ Phase III/src/student_orderUI.java
@@ -0,0 +1,57 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+
+public class student_orderUI {
+
+    @FXML
+    private RadioButton cheeseButton;
+
+    @FXML
+    private CheckBox extraButton;
+
+    @FXML
+    private ToggleGroup group;
+
+    @FXML
+    private CheckBox mushroomButton;
+
+    @FXML
+    private CheckBox olivesButton;
+
+    @FXML
+    private CheckBox onionButton;
+
+    @FXML
+    private TextArea orderTextArea;
+
+    @FXML
+    private AnchorPane parentPane;
+
+    @FXML
+    private RadioButton pepperoniButton;
+
+    @FXML
+    private Label pizzaTypeLabel;
+
+    @FXML
+    private Button placeOrderButton;
+
+    @FXML
+    private Label summaryLabel;
+
+    @FXML
+    private Label toppingLabel;
+
+    @FXML
+    private TextArea totalTextArea;
+
+    @FXML
+    private RadioButton vegButton;
+
+}
